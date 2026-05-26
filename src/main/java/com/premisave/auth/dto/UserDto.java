e30082af64d0c5aabb3773faf83d8f13fd06a4fb
Bill Graham Peacemaker <billgrahamp@gmail.com>
2026-05-27 02:32:29 +0300

    Refractored and removed unused code snippets

--- src/main/java/com/premisave/auth/dto/UserDto.java
+++ src/main/java/com/premisave/auth/dto/UserDto.java
@@ -1,26 +1,50 @@
 package com.premisave.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.premisave.auth.enums.Language;
 import com.premisave.auth.enums.Role;
 import lombok.Data;
 
 @Data
+@JsonPropertyOrder({
+    "id", "username", "email",
+    "firstName", "middleName", "lastName",
+    "phoneNumber", "country", "address1", "address2",
+    "language", "profilePictureUrl",
+    "role",
+    "active", "verified", "archived",
+    "password"
+})
 public class UserDto {
+
+    // Identification
     private String id;
-    private String username; 
+    private String username;
+    private String email;
+
+    // Personal Information
     private String firstName;
     private String middleName;
     private String lastName;
-    private String email;
+
+    // Contact & Location
     private String phoneNumber;
+    private String country;
     private String address1;
     private String address2;
-    private String country;
+
+    // Preferences
     private Language language;
     private String profilePictureUrl;
+
+    // Authorization
     private Role role;
+
+    // Account Status
     private boolean active;
     private boolean verified;
     private boolean archived;
-    private String password;           // Only for updates (should be null in responses)
+
+    // Security (should always be null in responses)
+    private String password;
 }
