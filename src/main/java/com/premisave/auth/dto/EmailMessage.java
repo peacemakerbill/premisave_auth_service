72bab8c57a5486a0d690a2874772322fae20963b
Bill Graham Peacemaker <billgrahamp@gmail.com>
2026-05-27 21:46:52 +0300

    fix(rabbitmq): fix EmailMessage deserialization security error

--- src/main/java/com/premisave/auth/dto/EmailMessage.java
+++ src/main/java/com/premisave/auth/dto/EmailMessage.java
@@ -10,6 +10,9 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmailMessage implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     private String to;
     private String subject;
     private String content;
